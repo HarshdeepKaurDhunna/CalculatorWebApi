a30db4d647028853c760cc91a25b2808aaafc410
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-07-20 02:15:32 -0400

    Add Methods for Calculation with handle exception in the user input wrong arguments

---
+++ Calculator-Logic/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator_Logic
+{
+    public class Calculator
+    {
+       
+        public double Addition(double left, double right)
+        {
+        
+            try
+            {
+                return left + right;
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public double Subtraction(double left, double right)
+        {
+
+            try
+            {
+                return left - right;
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public double Multiplication(double left, double right)
+        {
+
+            try
+            {
+                return left * right;
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException();
+            }
+        }
+        public double Divide(double left, double right)
+        {
+            if (right != 0)
+            {
+                return left / right;
+            }
+            else
+            {
+                throw new DivideByZeroException();
+            }
+
+        }
+    }
+}
