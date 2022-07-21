35c931cbe4660439c1eb71bf863c65ced4d8d63d
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-07-21 17:00:55 -0400

    Add Comments and Handle exception to prevent app from crash

--- CalculatorWebApi/Controllers/CalculatorController.cs
+++ CalculatorWebApi/Controllers/CalculatorController.cs
@@ -24,38 +24,82 @@ namespace CalculatorWebApi.Controllers
         /*
          *@Method Type:  Get 
          *@Action: Add
+         *@parameters: two Numbers
+         *This api is used get the result of addition of two numbers
          */
         [HttpGet("Add")]
         public IActionResult GetAddResult(double left, double right)
         {
-           return Ok(Calc.Addition(left, right));
-            
+          
+            try
+            {
+                return Ok(Calc.Addition(left, right)); // call to class library Addition Method and return success
+            }
+            catch (Exception)
+            {
+                return BadRequest(); // return bad request if any exception occur to save the application from crashing
+            }
 
         }
 
+        /*
+         *@Method Type:  Get 
+         *@Action: Subtract
+         *@parameters: two Numbers
+         *This api is used get the result of Subtraction of two numbers
+         */
         [HttpGet("Subtract")]
         public IActionResult getSubtractionResult(double left, double right)
         {
-            return Ok(Calc.Subtraction(left, right));
+            
+            try
+            {
+                return Ok(Calc.Subtraction(left, right)); // call to class library Subtraction Method and return success
+            }
+            catch (Exception)
+            {
+                return BadRequest(); // return bad request if any exception occur to save the application from crashing
+            }
 
         }
 
+        /*
+         *@Method Type:  Get 
+         *@Action: Multiply
+         *@parameters: two Numbers
+         *This api is used get the result of Multiplication of two numbers
+         */
         [HttpGet("Multiply")]
         public IActionResult getMultiplyResult(double left, double right)
         {
-            return Ok(Calc.Multiplication(left, right));
+          
+            try
+            {
+                return Ok(Calc.Multiplication(left, right)); // call to class library Multiplication Method and return success
+            }
+            catch (Exception)
+            {
+                return BadRequest(); // return bad request if any exception occur to save the application from crashing
+            }
 
         }
+
+        /*
+         *@Method Type:  Get 
+         *@Action: Division
+         *@parameters: two Numbers
+         *This api is used get the result of Division of two numbers
+         */
         [HttpGet("Division")]
         public IActionResult getDivisionResult(double left, double right)
         {
             try
             {
-                return Ok(Calc.Divide(left, right));
+                return Ok(Calc.Divide(left, right)); // call to class library Divide Method and return success
             }
-            catch (DivideByZeroException e)
+            catch (DivideByZeroException)
             {
-                return BadRequest();
+                return BadRequest(); // return bad request if any exception occur to save the application from crashing
 
             }
 
